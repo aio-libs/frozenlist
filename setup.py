607dfd12d5bf1291e22ec9e06607676f52f40c96
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-10-12 22:14:05 +0300

    Refactor setup.py to make it compatible with Python 3.9

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@ import sys
 
 from setuptools import Extension, setup
 
-try:
-    from Cython.Build import cythonize
-    CYTHON = True
-except ImportError:
-    CYTHON = False
-
 
 if sys.version_info < (3, 6):
     raise RuntimeError("frozenlist 1.x requires Python 3.6+")
@@ -23,36 +17,19 @@ NO_EXTENSIONS = (
 
 here = pathlib.Path(__file__).parent
 
-if CYTHON:
-    # convert .pyx files to .c files
-    # this happens for pure-python builds too because
-    # we'd want to be able to include the .c file
-    # in source distributions.
-    modules = cythonize('frozenlist/_frozenlist.pyx')
-else:
-    # No Cython, fall back to a distributed .c source
-    # file, if available.
-    c_source = here / 'frozenlist' / '_frozenlist.c'
-    if c_source.exists():
-        modules = [
-            Extension('frozenlist._frozenlist', [str(c_source)])
-        ]
-    else:
-        if not NO_EXTENSIONS:
-            print(
-                "Cython not available to produce .c file\n"
-                "Falling back to pure Python build.\n"
-            )
-        NO_EXTENSIONS = True
-
 if NO_EXTENSIONS:
     print("*********************")
     print("* Pure Python build *")
     print("*********************")
     ext_modules = None
-
 else:
-    ext_modules = modules
+    print("**********************")
+    print("* Accellerated build *")
+    print("**********************")
+    extensions = [
+        Extension('frozenlist._frozenlist', ['frozenlist/_frozenlist.c'])
+    ]
+
 
 txt = (here / 'frozenlist' / '__init__.py').read_text('utf-8')
 try:
@@ -84,6 +61,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Development Status :: 5 - Production/Stable',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
