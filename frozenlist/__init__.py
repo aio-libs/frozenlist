f36f5af38419789c10b21eade4e2174ef3beb346
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-10-15 15:19:03 +0300

    Bump to 1.2.0a1

--- frozenlist/__init__.py
+++ frozenlist/__init__.py
@@ -5,7 +5,7 @@ from collections.abc import MutableSequence
 from functools import total_ordering
 from typing import Tuple, Type
 
-__version__ = '1.2.0a0'
+__version__ = '1.2.0a1'
 
 __all__ = (
     'FrozenList', 'PyFrozenList'
