09189ad143fad4fd2861842d7ebd399744308be8
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-18 15:32:29 +0200

    Convert to declarative setup (#275)

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 import os
-import pathlib
-import re
 import sys
 
 from setuptools import Extension, setup
-
-if sys.version_info < (3, 6):
-    raise RuntimeError("frozenlist 1.x requires Python 3.6+")
-
 
 NO_EXTENSIONS = (
     bool(os.environ.get('FROZENLIST_NO_EXTENSIONS')) or
     sys.implementation.name != "cpython"
 )
-
-here = pathlib.Path(__file__).parent
 
 if NO_EXTENSIONS:
     print("*********************")
@@ -30,62 +22,6 @@ else:
     ]
 
 
-txt = (here / 'frozenlist' / '__init__.py').read_text('utf-8')
-try:
-    version = re.findall(r'^__version__ = "([^"]+)"\r?$',
-                         txt, re.M)[0]
-except IndexError:
-    raise RuntimeError('Unable to determine version.')
-
-install_requires = []
-
-
-def read(f):
-    return (here / f).read_text('utf-8').strip()
-
-
 setup(
-    name='frozenlist',
-    version=version,
-    description=(
-        'A list-like structure which implements '
-        'collections.abc.MutableSequence'
-    ),
-    long_description=read('README.rst'),
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Development Status :: 5 - Production/Stable',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-    ],
-    author='Nikolay Kim',
-    author_email='fafhrd91@gmail.com',
-    maintainer='Martijn Pieters <mj@zopatista.com>',
-    maintainer_email='aio-libs@googlegroups.com',
-    url='https://github.com/aio-libs/frozenlist',
-    project_urls={
-        'Chat: Gitter': 'https://gitter.im/aio-libs/Lobby',
-        'CI: Github Actions':
-            'https://github.com/aio-libs/frozenlist/actions',
-        'Coverage: codecov': 'https://codecov.io/github/aio-libs/frozenlist',
-        'Docs: RTD': 'https://frozenlist.readthedocs.io',
-        'GitHub: issues': 'https://github.com/aio-libs/frozenlist/issues',
-        'GitHub: repo': 'https://github.com/aio-libs/frozenlist',
-    },
-    license='Apache 2',
-    packages=['frozenlist'],
     ext_modules=ext_modules,
-    python_requires='>=3.7',
-    install_requires=install_requires,
-    include_package_data=True,
-    exclude_package_data={"": ["*.c"]},
 )
